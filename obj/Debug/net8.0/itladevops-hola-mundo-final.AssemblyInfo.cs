c9662ecc7c0c7d7ea47d89e4ded0c11524ecb167
APROMASTER <arielnunezmendez@gmail.com>
2024-04-11 15:32:30 -0400

    Deployment change

--- obj/Debug/net8.0/itladevops-hola-mundo-final.AssemblyInfo.cs
+++ obj/Debug/net8.0/itladevops-hola-mundo-final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("itladevops-hola-mundo-final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fe2c0374050fb61edbe86ec81afec4df8fa7008")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50f593bda7369cc4503dda3f766e87a70543cf3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("itladevops-hola-mundo-final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("itladevops-hola-mundo-final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
